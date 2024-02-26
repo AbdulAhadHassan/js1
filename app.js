1c22e3ca47cf163640e5f2b4aeb37c5fe7a99212
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-02-26 22:36:02 +0400

    js1

---
+++ app.js
@@ -0,0 +1,113 @@
+
+
+// chapter 1
+
+
+// Q1
+// alert(`welcome`)
+
+
+// Q2
+// alert(`Error! Please enter a valid password`)
+
+
+
+// Q3
+// alert(`welcome to js land... \nHappy code!`)
+
+
+
+// Q4
+// window.alert(`welcome to js land...`)
+
+
+// Q5
+// window.alert(`hello im running this through web console`)
+
+
+
+
+
+// chapter 2
+
+// Q1
+// var myname 
+
+// Q2
+// myname ='abdulahad'
+
+// Q3
+// var message;
+// message = 'hello world';
+// alert(message);
+
+
+// Q4
+// var name = ('jhone doe');
+// var age = ('15 years old');
+// var field = ('certified mobile app development');
+
+// alert(name)
+// alert(age)
+// alert(field)
+
+
+
+// Q5
+// var food = ('pizza')
+// alert(' pizza \n pizz \n piz \n pi \n p')
+
+// Q6
+// var emailstart =('something')
+// var emailend = '@email.com'
+// var email = emailstart.concat(emailend)
+// alert(email)
+
+
+// Q7
+// var book = 'A smarter way to learn JavaScript';
+// alert(book);
+
+
+
+// Q8
+// document.getElementById('h1').textContent = `yay i can something something js`;
+
+
+// Q9
+// var thingy = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+// alert(thingy)
+
+
+
+
+// chapter 3
+
+// Q1
+// var agemain = ('im 16 years old');
+// alert(agemain)
+
+// Q2
+// var visited =  14
+// var time = ' times'
+// var sen = ' u have visited this website ';
+// let website = sen + visited + time ;
+// alert(website)
+
+// Q3
+
+// var birthyear = prompt('birth year');
+// var sent = ' my birth year is '
+// var birth = sent + birthyear  
+
+// document.getElementById("h1").textContent =  sent + birthyear
+ 
+
+// Q4
+// var name = prompt('your name');
+// var size = prompt('t-shit size you want');
+
+// document.getElementById("h1").textContent =  name + ' ordered 5 T-shirt(' + size + ') on XYZ Clothing store'
+
+
+
